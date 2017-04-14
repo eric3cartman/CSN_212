0f241bba77a915d4b46ee9482e1f21f7350e7ada
imatul <singhatul3797@gmail.com>
2017-04-15 03:15:46 +0530

    Update graham_scan.cpp

--- graham_scan.cpp
+++ graham_scan.cpp
@@ -82,6 +82,7 @@ void graham_scan(vector<pair<int, int> > &P)    {
 
 int main()
 {
+	//NUMBER OF INPUTS MUST BE GREATER THAN 3
 	vector<pair<int, int> > Points;
 	Points.pb(mp(3,3));
 	Points.pb(mp(3,-3));
